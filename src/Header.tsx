6f901a8cd4199d0cc6a8c97fa2005aa9ddc22e1e
Rahul office <rahul25111999@gmail.com>
2024-07-18 19:36:44 +0530

    commit clear error

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,27 +1,30 @@
-import {FC} from 'react'
+import { FC } from 'react';
 
+interface HeaderProps {
+    name?: string;
+}
 
-
-const Header: FC = () => {
-    return <nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">CRUD - React</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Options</a>
-          </li>
-          
-        </ul>
-      </div>
-    </div>
-  </nav>;
+const Header: FC<HeaderProps> = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">CRUD - React</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Options</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
-export default Header
+export default Header;
--- src/form.tsx
+++ src/form.tsx
@@ -3,18 +3,31 @@ import Header from './Header';
 
 const Form: FC = () => {
     const [namefield, setname] = useState('');
+    const [submittedName, setSubmittedName] = useState<string | null>(null);
 
-    const handleinput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
         setname(event.target.value);
     };
 
+    const handleSubmit = () => {
+        if (namefield.trim() !== '') {
+            setSubmittedName(namefield);
+        }
+    };
+
     return (
-        <><div>
-            <input type="text" name="namefield" value={namefield} placeholder="Enter your Name" onChange={handleinput} />
-            {/* <button onClick={} */}
-        </div>
-            <div style={{ marginTop: '10px' }}>
-                {namefield && <Header name={namefield} />}
+        <>
+            <Header name={submittedName || ''} />
+            <div style={{ margin: '10px 0' }}>
+                <input 
+                    type="text" 
+                    name="namefield" 
+                    value={namefield} 
+                    placeholder="Enter your Name" 
+                    onChange={handleInput} 
+                    style={{ marginBottom: '10px' }} 
+                />
+                <button onClick={handleSubmit}>Submit</button>
             </div>
         </>
     );
